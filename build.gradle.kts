f9d6f0765508a232c9c98b318e32dc7dd1ddf035
Eduardo José <duduxss3@gmail.com>
2022-02-23 21:02:31 -0300

    master
    	:heavy_plus:sign: Adding eureka client
    
    		 - Upgrade spring boot version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("plugin.jpa") version "1.4.32"
-    id("org.springframework.boot") version "2.6.1"
+
+    id("org.springframework.boot") version "2.6.3"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
+
     kotlin("jvm") version "1.6.0"
+    kotlin("plugin.jpa") version "1.4.32"
     kotlin("plugin.spring") version "1.6.0"
     kotlin("plugin.allopen") version "1.4.31"
 }
@@ -12,28 +14,7 @@ plugins {
 group = "com.ead"
 version = "0.0.1-SNAPSHOT"
 
-allOpen {
-    annotation("javax.persistence.Entity")
-}
-
-dependencies {
-    implementations(
-        "org.springframework.boot:spring-boot-starter-web",
-        "org.springframework.boot:spring-boot-starter-data-jpa",
-        "org.springframework.boot:spring-boot-starter-validation",
-
-        "com.fasterxml.jackson.module:jackson-module-kotlin",
-        "org.jetbrains.kotlin:kotlin-reflect",
-        "org.jetbrains.kotlin:kotlin-stdlib-jdk8",
-
-        "io.github.microutils:kotlin-logging-jvm:2.1.20",
-        "net.kaczmarzyk:specification-arg-resolver:2.6.2"
-    )
-
-    runtimeOnly("org.postgresql:postgresql:42.2.24")
-
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-}
+extra["springCloudVersion"] = "2021.0.1"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_11
@@ -55,6 +36,36 @@ tasks {
 
     withType<Test> {
         useJUnitPlatform()
+    }
+}
+
+allOpen {
+    annotation("javax.persistence.Entity")
+}
+
+dependencies {
+    implementations(
+        "org.springframework.boot:spring-boot-starter-web",
+        "org.springframework.boot:spring-boot-starter-data-jpa",
+        "org.springframework.boot:spring-boot-starter-validation",
+        "org.springframework.cloud:spring-cloud-starter-netflix-eureka-client",
+
+        "com.fasterxml.jackson.module:jackson-module-kotlin",
+        "org.jetbrains.kotlin:kotlin-reflect",
+        "org.jetbrains.kotlin:kotlin-stdlib-jdk8",
+
+        "io.github.microutils:kotlin-logging-jvm:2.1.20",
+        "net.kaczmarzyk:specification-arg-resolver:2.6.2"
+    )
+
+    runtimeOnly("org.postgresql:postgresql:42.2.24")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
     }
 }
 
